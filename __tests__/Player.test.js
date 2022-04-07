da20fa7e32e9147487e4c4de333bf069e25acf76
ewhite <ew117mc@yahoo.com>
2022-04-07 15:39:42 -0700

    test battle

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,7 @@
-const { TestWatcher } = require('jest');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
 
 test('create a player object', () => {
     const player = new Player('Dave');
@@ -9,9 +10,7 @@ test('create a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-    );
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
 
